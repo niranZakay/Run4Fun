49209a639a57abd53ce03f0a01cf9b3bfa5140d1
idantok <50590151+idantok@users.noreply.github.com>
2021-06-19 12:46:31 +0300

    add activities and finish main activity

--- loginActivity/app/src/main/java/com/example/run4fun/activities/MainActivity.java
+++ loginActivity/app/src/main/java/com/example/run4fun/activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.run4fun.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.run4fun.R;
 
@@ -12,5 +15,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //click to move StatisticalAnalysisActivity
+        final Button statisticalAnalyzeButton = (Button) findViewById(R.id.statistical_analyze_button);
+        statisticalAnalyzeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,StatisticalAnalysisActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //click to move WorkOutHistoryActivity
+        final Button acitivityHistoryButton = (Button) findViewById(R.id.acitivity_history_button);
+        acitivityHistoryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,WorkOutHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //click to move WorkOutActivity
+        final Button button = (Button) findViewById(R.id.acitivity_history_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,WorkOutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // click to move SettingsActivity
+        final Button settingsButton = (Button) findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //click to move WorkOutActivity
+        final Button startActivityButton = (Button) findViewById(R.id.start_activity_button);
+        startActivityButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,WorkOutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- loginActivity/app/src/main/java/com/example/run4fun/activities/WorkOutActivity.java
+++ loginActivity/app/src/main/java/com/example/run4fun/activities/WorkOutActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.run4fun.R;
+
 public class WorkOutActivity extends AppCompatActivity {
 
     @Override
