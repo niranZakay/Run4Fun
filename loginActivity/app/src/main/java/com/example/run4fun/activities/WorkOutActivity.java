014c9878b07ed24c2a42e39cd07119bf4e57a2d5
idantok <50590151+idantok@users.noreply.github.com>
2021-06-20 00:36:06 +0300

    add workout actiivty with out map

--- loginActivity/app/src/main/java/com/example/run4fun/activities/WorkOutActivity.java
+++ loginActivity/app/src/main/java/com/example/run4fun/activities/WorkOutActivity.java
@@ -1,16 +1,38 @@
 package com.example.run4fun.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 import com.example.run4fun.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class WorkOutActivity extends AppCompatActivity {
-
+    private MapView mapView;
+    private GoogleMap googleMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_out);
+
     }
+
+
+
 }
