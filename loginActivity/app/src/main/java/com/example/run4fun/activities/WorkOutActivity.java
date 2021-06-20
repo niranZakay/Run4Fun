662233fadef947857def3b2e035dc4f1c92dd16b
idantok <50590151+idantok@users.noreply.github.com>
2021-06-20 18:44:55 +0300

    add vibration

--- loginActivity/app/src/main/java/com/example/run4fun/activities/WorkOutActivity.java
+++ loginActivity/app/src/main/java/com/example/run4fun/activities/WorkOutActivity.java
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -220,6 +222,9 @@ public class WorkOutActivity extends AppCompatActivity implements OnMapReadyCall
 
     public void createAlertBeforeFinish() {
 
+        //make vibration
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(300);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setTitle(getString(R.string.alert_text));
