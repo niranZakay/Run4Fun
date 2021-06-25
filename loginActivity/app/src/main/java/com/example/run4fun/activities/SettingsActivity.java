1444cc3aa1cdf3fc1c00debe51230e06709190a2
idantok <50590151+idantok@users.noreply.github.com>
2021-06-25 15:38:19 +0300

    add spinner to settings

--- loginActivity/app/src/main/java/com/example/run4fun/activities/SettingsActivity.java
+++ loginActivity/app/src/main/java/com/example/run4fun/activities/SettingsActivity.java
@@ -10,8 +10,10 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.run4fun.R;
@@ -38,15 +40,42 @@ public class SettingsActivity extends AppCompatActivity {
         // Construct the data source
         ArrayList<SettingItem> arrayOfSettingItem= new ArrayList<SettingItem>();
         //add items to listView
-        arrayOfSettingItem.add(new SettingItem(getString(R.string.user_text), Pref.getValue(this,"user", "user")));
-        arrayOfSettingItem.add(new SettingItem(getString(R.string.height_text),Pref.getValue(this,"height", "height")));
-        arrayOfSettingItem.add(new SettingItem(getString(R.string.weight_text),Pref.getValue(this,"weight", "weight")));
-        arrayOfSettingItem.add(new SettingItem(getString(R.string.age_text),Pref.getValue(this,"age", "age")));
+        arrayOfSettingItem.add(new SettingItem(getString(R.string.height_text),Pref.getValue(this,"height", "175")));
+        arrayOfSettingItem.add(new SettingItem(getString(R.string.weight_text),Pref.getValue(this,"weight", "70")));
+        arrayOfSettingItem.add(new SettingItem(getString(R.string.age_text),Pref.getValue(this,"age", "24")));
+        arrayOfSettingItem.add(new SettingItem(getString(R.string.bmr_text),Pref.getValue(this,"BMR", "24")));
         // Create the adapter to convert the array to views
         SettingsAdapter adapter = new SettingsAdapter(this, arrayOfSettingItem);
         // Attach the adapter to a ListView
         ListView listView = (ListView) findViewById(R.id.settings_listview);
         listView.setAdapter(adapter);
+
+        //get the spinner from the xml.
+        Spinner genderSpinner = findViewById(R.id.spinner);
+//create a list of items for the spinner.
+        String[] items = new String[]{getString(R.string.male_text),getString(R.string.female_text)};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapterForSpinner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        genderSpinner.setAdapter(adapterForSpinner);
+        genderSpinner.setPrompt(Pref.getValue(this,"gender", "Male"));
+
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                                    @Override
+                                                    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                                                        Spinner genderSpinner = findViewById(R.id.spinner);
+                                                        Pref.setValue(getApplicationContext(),"gender",parentView.getItemAtPosition(position).toString());
+                                                        genderSpinner.setPrompt(Pref.getValue(getApplicationContext(),"gender", parentView.getItemAtPosition(position).toString()));
+                                                        Toast.makeText(getApplicationContext(),"'"+"Gender" +"' change to '"+parentView.getItemAtPosition(position).toString()+"' successfully",Toast.LENGTH_SHORT).show();
+//                                                        restartActivity();
+                                                    }
+
+                                                    @Override
+                                                    public void onNothingSelected(AdapterView<?> parentView) {
+                                                        // your code here
+                                                    }
+                                                });
 
         //add listner for click on the list
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -54,30 +83,26 @@ public class SettingsActivity extends AppCompatActivity {
             {
                 Log.i(TAG, "click on listview with position: " +String.valueOf(itemPosition));
                 switch (itemPosition) {
-                    //user
-                    case 0:
-                        {
 
-                    }
-                    break;
                     //height
-                    case 1:
+                    case 0:
                     {
                         createAlertDialogToSettingItem(getString(R.string.height_text),getString(R.string.please_enter_text) +" "+getString(R.string.height_text));
                     }
                         break;
                     //weight
-                    case 2:
+                    case 1:
                     {
                         createAlertDialogToSettingItem(getString(R.string.weight_text),getString(R.string.please_enter_text)+" "+getString(R.string.weight_text));
                     }
                         break;
                     //age
-                    case 3:
+                    case 2:
                     {
                         createAlertDialogToSettingItem(getString(R.string.age_text),getString(R.string.please_enter_text )+" "+getString(R.string.age_text));
                     }
                     break;
+
                     default:
                         break;
                 }
